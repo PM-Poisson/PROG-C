22631225ab3b87e6c5f750afb27e2949503150d1
Paul-Malo <paul-malo.poisson@cpe.fr>
2025-09-15 09:58:31 +0200

    bonjour

--- bonjour.c
+++ bonjour.c
@@ -1,12 +1,10 @@
 /* Fichier: hello.c
-* affiche 'Bonjour le Monde!!!' à l'écran.
+* affiche 'Hello World !' à l'écran.
 * auteur: Paul-Malo Poisson
-* Ceci est un commentaire sur plusieurs lignes
 */
 
-#include <stdio.h> // headers
+#include <stdio.h>
 
-// Ceci est un commentaire sur une ligne
 int main() {
     printf("Hello World !");
     return 0;
