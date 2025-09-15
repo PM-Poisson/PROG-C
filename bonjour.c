b0e60eb62d6bf6942fed90988f274de430cb690c
Paul-Malo <paul-malo.poisson@cpe.fr>
2025-09-15 08:53:03 +0200

    creating hello.c

---
+++ bonjour.c
@@ -0,0 +1,13 @@
+/* Fichier: bonjour1.c
+* affiche 'Bonjour le Monde!!!' à l'écran.
+* auteur: John Samuel
+* Ceci est un commentaire sur plusieurs lignes
+*/
+
+#include <stdio.h> // headers
+
+// Ceci est un commentaire sur une ligne
+int main() {
+    printf("Bonjour le Monde !!!");
+    return 0;
+}
